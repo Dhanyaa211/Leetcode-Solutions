713d50a5545cfa4ff01f0a4f71213c3085608ae5
Dhanyaa RS <dhanyaa211@gmail.com>
2026-04-07 20:17:13 +0530

    Time: 1 ms (99.98%), Space: 81.1 MB (6.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,15 +3,12 @@ class Solution {
         int left = 0;
         int sum = 0;
         int maxSum = Integer.MIN_VALUE;
-
-        for (int right = 0; right < nums.length; right++) {
-            sum += nums[right];                 // expand window
-            maxSum = Math.max(maxSum, sum);     // update answer
-
-            // shrink window if sum becomes negative
-            if (sum < 0) {
-                sum = 0;
-                left = right + 1;
+        for(int right=0;right<nums.length;right++){
+            sum+=nums[right];
+            maxSum=Math.max(maxSum,sum);
+            if(sum<0){
+                sum=0;
+                left=right+1;
             }
         }
         return maxSum;
